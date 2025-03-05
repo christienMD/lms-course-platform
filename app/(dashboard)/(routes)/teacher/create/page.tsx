061e1011f97e76b7d9a46ef759fc820eb0e5eb8a
MD <mdchristien12@gmail.com>
2025-03-05 10:41:14 +0100

    feat: build create form

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -1,7 +1,96 @@
 "use client";
 
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import toast from "react-hot-toast";
+
+const formSchema = z.object({
+  title: z.string().min(1, { message: "Title is required" }),
+});
+
+type FormData = z.infer<typeof formSchema>;
+
 const CreatePage = () => {
-  return <div>CreatePage</div>;
+  const router = useRouter();
+  const form = useForm<FormData>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+    },
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+  const onSubmit = async (values: FormData) => {
+    try {
+      const response = await axios.post("/api/courses", values);
+      router.push(`/teacher/courses/${response.data.id}`);
+    } catch {
+      toast.error("Something went wrong");
+    }
+  };
+
+  return (
+    <div className="max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6">
+      <div className="">
+        <h1 className="text-2xl">Name your Course</h1>
+        <p>
+          What would you like to name your course? , Don&apos;t worry , you can
+          change this later.{" "}
+        </p>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8 mt-8"
+          >
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Course title</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder="e.g 'Advdanced we development'"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    What will you teach in this course?
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+
+            <div className="flex items-center gap-x-2">
+              <Button asChild type="button" variant="ghost">
+                <Link href="/">Cancel</Link>
+              </Button>
+              <Button type="submit" disabled={isSubmitting || !isValid}>
+                Continue
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default CreatePage;
