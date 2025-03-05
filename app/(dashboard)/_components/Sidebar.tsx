f1cc57bd742ded32298e308e62411127ea411250
MD <mdchristien12@gmail.com>
2025-03-05 08:44:52 +0100

    feat: build navbar

--- app/(dashboard)/_components/Sidebar.tsx
+++ app/(dashboard)/_components/Sidebar.tsx
@@ -3,14 +3,15 @@
 import React from "react";
 import Logo from "./Logo";
 import SidebarRoutes from "./SidebarRoutes";
+import { cn } from "@/lib/utils";
 
-const Sidebar = () => {
+const Sidebar = ({ className }: { className?: string }) => {
   return (
     <div className="h-full border-r flex flex-col overflow-y-auto">
       <div className="p-6">
         <Logo />
       </div>
-      <div className="flex flex-col w-full">
+      <div className={cn("flex flex-col w-full", className)}>
         <SidebarRoutes />
       </div>
     </div>
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Sidebar from "./_components/Sidebar";
+import Navbar from "./_components/Navbar";
 
 interface Props {
   children: React.ReactNode;
@@ -8,6 +9,9 @@ interface Props {
 const DashboardLayout = ({ children }: Props) => {
   return (
     <div className="h-full">
+      <div className="h-[80px] md:pl-60 fixed w-full z-50 inset-y-0">
+        <Navbar />
+      </div>
       <div className="hidden md:flex h-full w-60 flex-col fixed inset-y-0 z-50">
         <Sidebar />
       </div>
