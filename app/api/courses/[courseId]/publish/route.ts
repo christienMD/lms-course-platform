58017755d778c911a92550ebfa6900b2f97336f1
MD <mdchristien12@gmail.com>
2025-03-12 07:23:44 +0100

    feat: Implement publish and unpunlish actions

---
+++ app/api/courses/[courseId]/publish/route.ts
@@ -0,0 +1,63 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params: { courseId } }: { params: { courseId: string } }
+) {
+  try {
+    const { userId } = await auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const course = await db.course.findUnique({
+      where: {
+        id: courseId,
+        userId: userId,
+      },
+      include: {
+        chapters: {
+          include: {
+            muxData: true,
+          },
+        },
+      },
+    });
+
+    if (!course) {
+      return new NextResponse("Course Not Found", { status: 404 });
+    }
+
+    const hasPublishedChapter = course.chapters.some(
+      (chapter) => chapter.isPublished
+    );
+
+    if (
+      !course.title ||
+      !course.description ||
+      !course.imageUrl ||
+      !course.categoryId ||
+      !hasPublishedChapter
+    ) {
+      return new NextResponse("Missing required fields", { status: 400 });
+    }
+
+    const publishedCourse = await db.course.update({
+      where: {
+        id: courseId,
+        userId,
+      },
+      data: {
+        isPublished: true,
+      },
+    });
+
+    return NextResponse.json(publishedCourse);
+  } catch (error) {
+    console.log("[COURSE_ID_PUBLISH]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
