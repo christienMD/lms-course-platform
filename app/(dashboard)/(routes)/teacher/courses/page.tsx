f4790998dfa2106dc973da6c8f45c3b105867280
MD <mdchristien12@gmail.com>
2025-03-13 09:22:15 +0100

    feat: setup tanstack react table feat: display all courses

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,13 +1,28 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import React from "react";
+import { auth } from "@clerk/nextjs/server";
+import { columns } from "./components/columns";
+import { DataTable } from "./components/data-table";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
 
-const CoursesPage = () => {
+const CoursesPage = async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
     <div className="p-6">
-      <Button asChild>
-        <Link href="/teacher/create">New Course</Link>
-      </Button>
+      <DataTable columns={columns} data={courses} />
     </div>
   );
 };
