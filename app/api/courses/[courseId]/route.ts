5eccbfd077ec9549d92466f73a451d95ddb7a3dc
MD <mdchristien12@gmail.com>
2025-03-16 18:27:39 +0100

    feat: protect api routes

--- app/api/courses/[courseId]/route.ts
+++ app/api/courses/[courseId]/route.ts
@@ -3,6 +3,7 @@ import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 import { db } from "@/lib/db";
+import { isTeacher } from '@/lib/teacher';
 
 const { video } = new Mux({
   tokenId: process.env.MUX_TOKEN_ID!,
@@ -16,7 +17,7 @@ export async function DELETE(
   try {
     const { userId } = await auth();
 
-    if (!userId) {
+    if (!userId || !isTeacher(userId)) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,12 +1,15 @@
+import { isTeacher } from "@/lib/teacher";
 import { auth } from "@clerk/nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
-import { UploadThingError } from "uploadthing/server";
 
 const f = createUploadthing();
 
 const handleAuth = async () => {
   const { userId } = await auth();
-  if (!userId) throw new Error("Unauthorized");
+
+  const isAuthorized = isTeacher(userId)
+
+  if (!userId || !isAuthorized) throw new Error("Unauthorized");
   return { userId };
 };
 
