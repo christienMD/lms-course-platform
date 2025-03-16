fd77d87fce76cd58594868297a4f0f048f49dae1
MD <mdchristien12@gmail.com>
2025-03-16 19:52:05 +0100

    fix: fix suspense boundary error

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,24 +1,25 @@
 import { db } from "@/lib/db";
-import React from "react";
+import React, { Suspense } from "react";
 import Categories from "./components/Categories";
 import SearchInput from "@/components/SearchInput";
 import { getCourses } from "@/actions/getCourses";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import CoursesList from "./components/CoursesList";
+import PageLoader from "@/components/PageLoader";
 
 interface Props {
   searchParams: {
     title: string;
-    categoryId: string
-  }
+    categoryId: string;
+  };
 }
 
-const SearchPage = async ({searchParams}:Props) => {
-  const {userId} = await auth()
+const SearchPage = async ({ searchParams }: Props) => {
+  const { userId } = await auth();
 
   if (!userId) {
-    return redirect('/')
+    return redirect("/");
   }
 
   const categories = await db.category.findMany({
@@ -29,13 +30,15 @@ const SearchPage = async ({searchParams}:Props) => {
 
   const courses = await getCourses({
     userId,
-    ...searchParams
-  })
+    ...searchParams,
+  });
 
   return (
     <>
       <div className="px-6 pt-6 md:hidden md:mb-0 block">
-        <SearchInput />
+        <Suspense fallback={<PageLoader />}>
+          <SearchInput />
+        </Suspense>
       </div>
       <div className="p-6">
         <Categories items={categories} />
